ec6bcd81312c0f8d0822f745827cee8a92e18fa6
oleg.hoefling <oleg.hoefling@gmail.com>
2021-08-26 13:39:24 +0200

    initial commit
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

---
+++ _typeshed/src/backoff.pyi
@@ -0,0 +1,3 @@
+from typing import Any, Generator
+
+def expo(base: int = ..., factor: int = ..., max_value: int | None = ...) -> Generator[int, Any, None]: ...
---
+++ src/main.py
@@ -0,0 +1,6 @@
+# src/main.py
+
+from backoff import expo
+from pydantic import BaseModel
+
+expo("fizz")
